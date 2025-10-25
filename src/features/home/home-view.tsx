5176499b2c8c990b4780491fa1b982834f4bbfe8
kramlj00 <kristinaramljak5@gmail.com>
2025-10-25 12:17:47 +0200

    Added lenis

--- src/features/home/home-view.tsx
+++ src/features/home/home-view.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect } from "react";
 import AboutUs from "./components/about-us";
 import Blog from "./components/blog";
 import Contact from "./components/contact";
@@ -5,8 +8,15 @@ import Gallery from "./components/gallery";
 import Hero from "./components/hero";
 import HomeCta from "./components/home-cta";
 import WineList from "./components/wine-list";
+import Lenis from "lenis";
 
 const HomeView = () => {
+  useEffect(() => {
+    new Lenis({
+      autoRaf: true,
+    });
+  }, []);
+
   return (
     <main>
       <Hero />
